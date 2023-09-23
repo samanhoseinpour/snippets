16aed86ee7da5e8f6973d027f4abeb2b5d6e1dd9
Saman <samangithoseinpour@gmail.com>
2023-09-23 16:04:00 +0330

    feat: handling user input

--- jest/users/src/components/UserForm.jsx
+++ jest/users/src/components/UserForm.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
+
 const UserForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(name, email);
+  };
+
   return (
-    <div className="flex flex-wrap justify-start flex-col gap-6 p-12">
+    <form
+      onSubmit={handleFormSubmit}
+      className="flex flex-col flex-wrap gap-6 p-12"
+    >
       <div>
         <label className="block text-sm font-medium leading-6 text-gray-900">
           Name
         </label>
         <input
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
           placeholder="Jhon Junior"
           className="rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         />
@@ -15,6 +32,9 @@ const UserForm = () => {
           Email
         </label>
         <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           placeholder="example@example.com"
           className="rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         />
@@ -24,7 +44,7 @@ const UserForm = () => {
       </button>
       <hr />
       <hr />
-    </div>
+    </form>
   );
 };
 

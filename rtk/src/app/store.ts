68d1f2bd5ddfac85dc1b5ecd370a17ce4533d702
Saman <samangithoseinpour@gmail.com>
2023-09-27 21:12:29 +0330

    connect rtk query to store

--- rtk/src/app/store.ts
+++ rtk/src/app/store.ts
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import { apiSlice } from '../features/api/dogsSliceApi';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(apiSlice.middleware);
   },
 });
 

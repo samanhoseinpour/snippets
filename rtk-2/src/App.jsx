649974124117eeca8e539a9dc55e72f164654f03
Saman <samangithoseinpour@gmail.com>
2023-09-28 10:38:01 +0330

    project setup

---
+++ rtk-2/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="bg-red-50 text-center font-bold text-orange-600 uppercase py-5">
+        redux toolkit second project
+      </h1>
+    </>
+  );
+};
+
+export default App;
